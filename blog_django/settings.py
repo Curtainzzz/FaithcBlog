0d3b79b1bfe3dcfcb6a4058a7b6695e1482996e3
lzc <895698141@qq.com>
2020-09-17 10:31:01 +0800

    修改mysql配置

--- blog_django/settings.py
+++ blog_django/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blog',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'lzc748596',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
